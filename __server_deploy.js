c629ef0a3de08362fb72ae7f3d4966b615536d4c
neloy005 <arifistiekofficial@gmail.com>
2022-04-26 00:46:16 +0600

    genius csr

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * --------------------------------
+ * ONE TIME or computer
+ * 1. Create heroku account
+ * 2. Verify email
+ * 3. Install heroku CLI
+ * 4. heroku login
+ * -------------------------
+ * For each project one time
+ * -------------------------
+ * 1. heroku create
+ * 2. make sure you: git add . git commit . git push
+ * 3. git push heroku main
+*/
